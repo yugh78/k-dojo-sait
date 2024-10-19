62b6a426df409d98501f54af4bb203f43178381d
Maestro <sargsyan.a917@gmail.com>
2024-10-19 13:51:25 +0300

    Front-init

---
+++ client/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import HelloWorld from '@/components/HelloWorld.vue'
+import TheWelcome from '@/components/TheWelcome.vue'
+import TheHeader from '@/components/TheHeader.vue'
+
+</script>
+
+<template>
+  <div>
+  <header>
+   
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <div></div>
+    <TheHeader></TheHeader>
+    <TheWelcome />
+  </main>
+</div>
+</template>
---
+++ client/src/components/TheHeader.vue
@@ -0,0 +1,6 @@
+<script setup>
+let s = ref(12);
+</script>
+<template>
+    <div class="p-3">{{ s }}</div>
+</template>
