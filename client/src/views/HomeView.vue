8fad55ffa5d3780681ba5b677f4cec3e582b85a2
Maestro <sargsyan.a917@gmail.com>
2024-10-23 20:04:08 +0300

    Delete node modules

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,23 +1,5 @@
 <template>
-  <form action="/" method="post">
-    <fieldset>
-      <legend>Заявка</legend>
-      <div>
-        <div><label>Ваше имя: <input type="text" name="token" placeholder="Введите Ваше имя" required></label></div>
-        <div><label>Ваша фамилия: <input type="text" name="token" placeholder="Введите Вашу фамилию" required></label></div>
-        <div><label>Ваша почта: <input type="email" name="token" placeholder="Введите Вашу почту" required></label></div>
-        <div><label>Ваш номер телефона: <input type="telephone" name="token" placeholder="Введите Ваш номер телефона" required></label>
-        </div>
-      </div>
-      <div>
-        <button type="submit">отправить</button>
-      </div>
-    </fieldset>
-  </form>
+  <div>
+    Home
+  </div>
 </template>
-
-<style>
-.TextArea {
-  width: 100%;
-}
-</style>
