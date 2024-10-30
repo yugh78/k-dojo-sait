c99530dd5dbf4db54cd9c2d0998e838cf1d64dd7
Maestro <sargsyan.a917@gmail.com>
2024-10-30 14:31:43 +0300

    проверка на заполненность и чуть изменил синтаксис

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,37 +1,46 @@
 <template>
-  <!-- <form  > -->
-    <fieldset>
-      <legend>Заявка</legend>
-      <div>
-        <div><label>Ваше имя: <input type="text" name="token" v-model="name" placeholder="Введите Ваше имя" required></label></div>
-        <div><label>Ваша почта: <input type="email" name="token" v-model="mail" placeholder="Введите Вашу почту" required></label></div>
-        <div><label>Ваш номер телефона: <input type="phone" name="token" v-model="phone" placeholder="Введите Ваш номер телефона" required></label>
-        </div>
+  
+  <fieldset>
+    <legend>Заявка</legend>
+    <div>
+      <div><label>Ваше имя: <input type="text" name="token" v-model="name" placeholder="Введите Ваше имя"
+            required></label></div>
+      <div><label>Ваш номер телефона: <input type="phone" name="token" v-model="phone"
+            placeholder="Введите Ваш номер телефона" required></label>
+        <div><label>Ваше сообщение: <textarea name="token" v-model="message" placeholder="Введите Ваше сообщение" id=""
+              cols="30" rows="10" wrap="soft" required></textarea></label></div>
       </div>
-      <div>
-        <button @click.prevent="subm()">отправить</button>
-      </div>
-    </fieldset>
-  <!-- </form> -->
+    </div>
+    <div>
+      <button @click.prevent="subm()">отправить</button>
+    </div>
+  </fieldset>
+  
 </template>
 
 <script setup>
 import { ref } from 'vue';
 
 let name = ref('');
-let mail = ref('');
 let phone = ref('');
+let message = ref('');
 
 const subm = async () => {
+  // Проверка на заполненность полей
+  if (!name.value || !phone.value || !message.value) {
+    console.error('Все поля должны быть заполнены.');
+    return; // Не отправляем запрос, если поля пустые
+  }
+
   const data = {
     name: name.value,
-    email: mail.value,
     phone: phone.value,
+    message: message.value,
   };
 
   try {
     // Отправляем POST-запрос на сервер
-    const response = await fetch('', {
+    const response = await fetch('http://127.0.0.1:8000/applications/create/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',  // Указываем, что отправляем JSON
@@ -60,3 +69,4 @@ const subm = async () => {
   width: 100%;
 }
 </style>
+
